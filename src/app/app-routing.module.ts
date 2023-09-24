1150b3eaa401fe06fbb9d7f1695ae9412d3f8848
Ashish Chandra <ashish.chandra8771@gmail.com>
2023-09-24 14:46:45 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'register', pathMatch:'full'},
+  {path: 'register', component: CreateComponent},
+  { path: 'update/:id', component: CreateComponent },
+  { path: 'detail/:id', component: UserDetailComponent },
+  {path:'list', component: RegistrationListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
